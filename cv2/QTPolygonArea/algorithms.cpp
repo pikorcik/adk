871fbbdf5b5133b4aa6751c3b22f6cb4d5c2d686
bayertom <bayertom@users.noreply.github.com>
2017-10-23 10:43:25 +0200

    Final version

--- cv2/QTPolygonArea/algorithms.cpp
+++ cv2/QTPolygonArea/algorithms.cpp
@@ -7,13 +7,16 @@ double Algorithms::LH(const std::vector<QPoint> &points)
     //Polygon area
     const unsigned int n = points.size();
     double area = 0;
-    for (int i = 1; i < n - 1; i++)
-    {
-        area += points[i].x() * (points[i+1].y() - points[i-1].y());
+    if(n>2){
+
+        for (int i = 1; i < n - 1; i++)
+        {
+             area += points[i].x() * (points[i+1].y() - points[i-1].y());
+        }
+
+         area+=points[0].x() * (points[1].y() - points[n-1].y());
+         area+=points[n-1].x() * (points[0].y() - points[n-2].y());
     }
 
-    area+=points[0].x() * (points[1].y() - points[n-1].y());
-    area+=points[n-1].x() * (points[0].y() - points[n-2].y());
-
-    return 0.5 * area;
+    return std::fabs(0.5 * area);
 }
